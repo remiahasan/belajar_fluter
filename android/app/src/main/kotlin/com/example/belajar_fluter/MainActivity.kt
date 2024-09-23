0b460f09bda3b961752939e7de06c20fc52c7f31
romi1274 <romiromet123@gmail.com>
2024-09-23 14:43:21 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/belajar_fluter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.belajar_fluter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/wiget_baru/texs_wiget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class TexsWiget extends StatelessWidget {
+  const TexsWiget({Key? key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return const Text("belajar fluter");
+  }
+}
